491f98af4d48149ab71435e7548510baa1102857
imamalif1616 <imamalif1616@gmail.com>
2022-10-20 12:22:51 +0700

    Add project files.

---
+++ WpfStopwatch/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfStopwatch.Core;
+
+namespace WpfStopwatch.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand Page1ViewCommand { get; set; }
+        public RelayCommand Page2ViewCommand { get; set; }
+        public RelayCommand Page3ViewCommand { get; set; }
+        public Page1ViewModel Page1VM { get; set; }
+        public Page2ViewModel Page2VM { get; set; }
+        public Page3ViewModel Page3VM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();    
+            }
+        }
+
+        public MainViewModel()
+        {
+            Page1VM = new Page1ViewModel();
+            Page2VM = new Page2ViewModel();
+            Page3VM = new Page3ViewModel();
+
+
+            CurrentView = Page1VM;
+
+            Page1ViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = Page1VM;
+            });
+
+            Page2ViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = Page2VM;
+            });
+
+            Page3ViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = Page3VM;
+            });
+        }
+    }
+}
---
+++ WpfStopwatch/MVVM/ViewModel/Page1ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfStopwatch.MVVM.ViewModel
+{
+    internal class Page1ViewModel
+    {
+        public List<string> NameCollection { get; set; }
+
+        public Page1ViewModel()
+        {
+            NameCollection = new List<string>()
+            {
+                "GET",
+                "PUT",
+                "POST",
+                "DELETE"
+            };
+        }
+    }
+}
