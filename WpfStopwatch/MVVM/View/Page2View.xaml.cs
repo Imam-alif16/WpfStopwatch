dd3b759d297225c8e7aee3cd5a21be585a9bb7f4
imamalif1616 <imamalif1616@gmail.com>
2022-10-29 20:01:51 +0700

    stopwatch sementara pakai tombol

--- WpfStopwatch/MVVM/View/Page2View.xaml.cs
+++ WpfStopwatch/MVVM/View/Page2View.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +22,55 @@ namespace WpfStopwatch.MVVM.View
     /// </summary>
     public partial class Page2View : UserControl
     {
+        private const string _startStopwatchDisplay = "00:00:00:00";
+        private Timer _timer;
+
+        int h, m, s, ms;
+
         public Page2View()
         {
             InitializeComponent();
+
+            StopwatchDisplay.Text = _startStopwatchDisplay;
+
+            _timer = new Timer(interval:1);
+            _timer.Elapsed += OnTimerElapse;
+        }
+
+        private void OnTimerElapse(object sender, ElapsedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                ms += 1;
+                if (ms == 100)
+                {
+                    ms = 0;
+                    s += 1;
+                }
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+
+                }
+
+                StopwatchDisplay.Text = String.Format("{0}:{1}:{2}:{3}", h.ToString().ToString().PadLeft(2, '0'), m.ToString().ToString().PadLeft(2, '0'), s.ToString().ToString().PadLeft(2, '0'), ms.ToString().ToString().PadLeft(2, '0'));
+            });
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
         }
     }
 }
